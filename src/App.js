b37fd38d12c076dde64cb40ba4913fda621ad2be
mohammed saeed <mo.elsaeed.aly@gmail.com>
2019-06-16 11:12:12 +0200

    removed all posts from this PR

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
 import { Header, Footer } from "./components/layout";
-import AllPosts from "./components/AllPosts/allPosts";
 
 function App() {
   return (
     <div>
       <Header />
-      <AllPosts />
       <Footer />
     </div>
   );
